09c8c5dfbb9b53c7dd0577d31fb245199ac817d3
Jasser Cherif <120514151+jasser-cherif@users.noreply.github.com>
2023-05-26 15:57:52 +0200

    Update sign.php

--- sign.php
+++ sign.php
@@ -1,21 +1,6 @@
 <?php
-//include('connexion.php');
-
-$host="localhost";
-$user="root";
-$password="root";
-$dbname="learn2drive";
-//$link=mysql_connect($host,$user,$password) or die ('impossible de se connceter');
-//mysql_select_db($dbname,$link);
 
 
-$mysqli = new mysqli($host,$user,"",$dbname);
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
 
 $nom =$_POST['nom'] ;
 $img =$_POST['img'] ;
@@ -27,13 +12,58 @@ $age =$_POST['age'] ;
 $email =$_POST['email'] ; 
 $ss =$_POST['ss'] ; 
 $cs =$_POST['cs'] ; 
-$lic =$_POST['lic'] ; 
+$lic =$_POST['lic'] ;
 
 
-$requete="insert into condidat(nom,img,db,pb,adr,pn,age,email,ss,cs,lic) values ('$nom', '$img', '$db', '$pb','$adr', '$pn', '$age', '$email', '$ss', '$cs', '$lic' );  ";
-if(mysqli_query($mysqli,$requete))
-echo 'insertion réussie<br> <a href="sign.html"> Retour </a>';
-else
-echo 'insertion non réussie<br> <a href="sign.html"> Retour </a>';
+
+
+$connexion=mysqli_connect("localhost", "root", "");
+if ($connexion)
+			{// Connexion au serveur effectuée
+			echo "connexion réussie";
+			$bd = mysqli_select_db($connexion, "learn2drive");
+			if ($bd)
+			{	
+				// Connexion à la base de données effectuée
+				echo "connexion à la BD réussie";
+				$requete = "INSERT INTO `condidat` (`nom`,`img`,`db`,`pb`,`adr`,`pn`,`age`,`email`,`ss`,`cs`,`lic`) values ('$nom','$img','$db','$pb','$adr','$pn','$age','$email','$ss','$cs','$lic')";
+				
+				if (isset($_POST['envoyer']))
+			{
+				/*echo "vous avez une alerte";
+				echo "$ss et $cs";*/
+				echo '<script>alert("enregistrer");</script>';
+				header("Location:registration.html");
+				$resultat = mysqli_query($connexion, $requete);
+
+				if ($resultat)
+				{	// requête exécutée
+					echo "Requête correcte";
+					/*$nbr_lignes = mysqli_num_rows($resultat);
+					echo "$nbr_lignes lignes extraites de la base";
+					while($ligne = mysqli_fetch_row($resultat)) 
+					{	
+						$log = $ligne[0];
+						$gp = $ligne[1];
+						echo $log. "-" .$gp." ";
+					}*/
+                    
+				} 
+				else 
+					echo "Requête incorrecte"; 
+			} 
+			else 
+				echo "<p> BD inconnue</p>";
+		}
+		} 
+		else 
+			echo "Erreur de connexion";
+
+
+
+
+
+
+
 
 ?>
